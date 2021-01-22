7fb65792ed72698e4d446ca9ded96f66c764c92e
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-01-22 19:03:06 -0300

    exercicio 02 bloco 18.1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import TrafficSignal from './TrafficSignal';
 import './App.css';
 import Cars from './Cars';
 
 function App() {
   return (
     <div className="container">
-      <Cars />
-      <TrafficSignal />
+      <Cars />     
     </div>
   );
 }
