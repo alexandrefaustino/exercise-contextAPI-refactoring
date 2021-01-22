f90873b33e404671825945bf7829809ce4918fc2
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-01-22 16:04:04 -0300

    criação do contexto: CarsContext.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Cars from './Cars';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends Component {
+  render() {
+    return(
+     <Cars />
+    )
+  }      
 }
 
 export default App;
