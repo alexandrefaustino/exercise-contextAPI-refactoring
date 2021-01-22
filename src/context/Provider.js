1c141f811911e1cdc446bc0480571cfa211793f9
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-01-22 17:54:28 -0300

    conclusão do exercício 01

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,2 +1,42 @@
 import React, {Component} from 'react';
+import CarsContext from './CarsContext';
 
+class Provider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+    }
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(car, side) {
+    this.setState({
+      cars: {
+        ...this.state.cars,
+        [car]: side,
+      },
+    });
+  };
+
+  render() {
+    const contextCar = {
+      ...this.state,
+      moveCar: this.moveCar,
+    };
+    
+    const { children } = this.props;
+
+    return(
+      <CarsContext.Provider value={ contextCar }>
+        { children }
+      </CarsContext.Provider>
+    );
+  }
+}
+
+export default Provider;
